443faa6b8f556cfcfe06eadae38ffd603436eed9
Rushal <rushalmanandhar@gmail.com>
2025-09-25 16:44:50 +0545

    if needs starting template

--- backend/routes/esewaRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { EsewaInitiatePayment, paymentStatus } from "../controllers/esewaController.js";
-const router = express.Router();
-
-router.post("/initiate-payment", EsewaInitiatePayment);
-router.post("/payment-status", paymentStatus);
-
-export default router;
